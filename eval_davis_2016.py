12a9c4ed5b854c44d279f90db6cf66a0b0e1f3f7
Fabien Merceron <f.merceron@yahoo.com>
2022-01-11 13:18:43 +0100

    minor change

--- eval_davis_2016.py
+++ eval_davis_2016.py
@@ -56,7 +56,8 @@ total_process_time = 0
 total_frames = 0
 
 print(len(test_loader))
-print(test_loader[0])
+test_features = next(iter(test_loader))
+print(test_features[0])
 
 # Start eval
 for data in progressbar(test_loader, max_value=len(test_loader), redirect_stdout=True):
